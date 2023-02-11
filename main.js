04ae245923d61b55ed9306c1aa1a34d0d64cf654
mathy <matiwordgit@gmail.com>
2023-02-11 11:27:08 -0300

    Limpieza de código

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 import 'tailwindcss/tailwind.css';
 import logowhite from './img/logo-white.png'
 import youtube from './img/youtube.png'
@@ -41,24 +40,3 @@ document.querySelector('#app').innerHTML = `
 </div>
 
 `
-
-/* document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
- */
